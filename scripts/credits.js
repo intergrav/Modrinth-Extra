7a09f06141568af1bba55f9e06f9d8e9795a2cbc
intergrav <devinkid1@gmail.com>
2022-08-16 17:41:27 -0400

    Update credits.js

--- scripts/credits.js
+++ scripts/credits.js
@@ -2,4 +2,4 @@
 // Created by intergrav on 8/16/2022
 
 var creditsText = document.createElement("p");
-creditsText.innerHTML = 'Modrinth Extra is <a href="https://github.com/intergrav/Modrinth-Extra">open source.'
+creditsText.innerHTML = 'Modrinth Extra is <a href="https://github.com/intergrav/Modrinth-Extra">open source</a>.'
