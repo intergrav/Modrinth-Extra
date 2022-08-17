e6b4ce7dc3fefd0c6b7384ecf2dceae1bacc03c6
intergrav <devinkid1@gmail.com>
2022-08-16 23:38:41 -0400

    remove popup js

--- scripts/popup.js
+++
@@ -1,7 +0,0 @@
-// Shows a page to the user with disclaimers
-
-chrome.runtime.onInstalled.addListener(function (object) {
-    if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-        chrome.tabs.create({url: chrome.runtime.getURL("popup/index.html")})
-    }
-});
