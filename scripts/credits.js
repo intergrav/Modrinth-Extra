ca40a5e4aee968456e0c7f56d07838c9ec2f9329
intergrav <devinkid1@gmail.com>
2022-08-16 22:31:19 -0400

    stuff

--- scripts/credits.js
+++ scripts/credits.js
@@ -1,13 +1,19 @@
 // This script appends credits to the bottom of the Modrinth page, linking to the ME repository.
 
-var modrinthInformation = document.getElementsByClassName("logo-info");
-let creditsText = document.createElement("p");
-let creditsCopyrightText = document.createElement("p");
+var modrinthInformation = document.getElementsByClassName("logo-info")
+let creditsImage = document.createElement("img")
+let creditsText = document.createElement("p")
+let creditsCopyrightText = document.createElement("p")
 
-creditsText.innerHTML = ' Modrinth Extra is <a target="_blank" href="https://github.com/intergrav/Modrinth-Extra" class="text-link"> open source</a>. ';
+creditsImage.src = chrome.runtime.getURL("branding/greyicontextbig_32h.png")
+creditsImage.height = "29"
+
+creditsText.innerHTML = ' Modrinth Extra is <a target="_blank" href="https://github.com/intergrav/Modrinth-Extra" class="text-link"> open source</a>. '
 
 creditsCopyrightText.innerHTML = '© intergrav'
 
-modrinthInformation[0].appendChild(creditsText);
+modrinthInformation[0].appendChild(creditsImage)
 
-modrinthInformation[0].appendChild(creditsCopyrightText);
+modrinthInformation[0].appendChild(creditsText)
+
+modrinthInformation[0].appendChild(creditsCopyrightText)
