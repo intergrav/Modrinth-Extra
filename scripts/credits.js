9c11637a51c9d147bd1ae107b51c9cc5b5586770
intergrav <devinkid1@gmail.com>
2022-08-17 18:12:24 -0400

    more branding

--- scripts/credits.js
+++ scripts/credits.js
@@ -1,11 +1,16 @@
 // This script appends credits to the bottom of the Modrinth page, linking to the ME repository.
 
+var isDarkMode = false
 var modrinthInformation = document.getElementsByClassName("logo-info")
 let creditsImage = document.createElement("img")
 let creditsText = document.createElement("p")
 let creditsCopyrightText = document.createElement("p")
 
-creditsImage.src = chrome.runtime.getURL("branding/greyicontextbig_32h.png")
+if (document.getElementsByTagName("html")[0].className.includes("dark-mode")) {
+    creditsImage.src = chrome.runtime.getURL("branding/dark/darker/icontextbig_svg.svg")
+} else {
+    creditsImage.src = chrome.runtime.getURL("branding/light/darker/icontextbig_svg.svg")
+}
 creditsImage.height = "30"
 
 creditsText.innerHTML = ' Modrinth Extra is <a target="_blank" href="https://github.com/intergrav/Modrinth-Extra" class="text-link"> open source</a>. '
